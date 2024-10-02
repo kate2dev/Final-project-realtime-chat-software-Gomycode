2429b8c7b43ad18f47ee8a346603e8c2c4b6e0d4
Kate2dev <eseemoefe@gmail.com>
2024-10-02 10:11:09 +0100

    updates....

--- backend/controllers/auth/authControllers.js
+++ backend/controllers/auth/authControllers.js
@@ -1,8 +1,7 @@
 const postLogin = require("./postLogin");
-const postRegister = require("./postRegister");
-
+const postRegister = require("./postRegister")
 
 exports.controllers = {
-    postLogin,
-    postRegister
+  postLogin,
+  postRegister,
 };
