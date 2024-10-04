0e1b5fb0d1c7243171f59583f6c26de5955797f9
Kate2dev <eseemoefe@gmail.com>
2024-10-04 03:26:57 +0100

    still working on login page...

--- frontend/src/authPages/LoginPage/LoginPage.jsx
+++ frontend/src/authPages/LoginPage/LoginPage.jsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import AuthBox from "../../shared/components/authbox";
 import LoginPageHeader from "./LoginPageHeader";
 import LoginPageInputs from "./LoginPageInputs";
+import LoginPageFooter from "./LoginPageFooter";
+import { validateLoginForm } from "../../shared/utils/validators";
 
+
+// /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
 const LoginPage = () => {
-  // 
 
   const [mail, setMail] = useState("");
   const [password, setPassword] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
+
+
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+  }, [mail, password, setIsFormValid]);
+
+
+  const handleLogin = () => {
+    const userDetails = {
+      mail,
+      password,
+    };
+
+    login(userDetails, history);
+  };
+
   return (
     <AuthBox>
       <LoginPageHeader />
@@ -18,6 +37,7 @@ const LoginPage = () => {
         password={password}
         setPassword={setPassword}
       />
+      <LoginPageFooter isFormValid={isFormValid} handleLogin={handleLogin} />
     </AuthBox>
   );
 };
