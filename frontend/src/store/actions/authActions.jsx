2dabb261a3a33971fac28f684c09b79af5789911
Kate2dev <eseemoefe@gmail.com>
2024-10-08 10:49:06 +0100

    Define auth actions and user detail setter
    Added authActions object with SET_USER_DETAILS action type.
    Created setUserDetails action creator for updating Redux store with user information.
    Implement login and registration actions with async API calls
    
    Added login and register async action creators.
    Integrated API calls with api.login and api.register for authentication flow.
    Updated Redux store with user details upon successful login/registration.
    Stored user details in localStorage and navigated to the dashboard after authentication.
    Bind action creators to dispatch for easier usage in components
    
    Implemented getActions to return bound action creators for login and register.
    Simplified action dispatching in components by binding login and register to dispatch.

--- frontend/src/store/actions/authActions.jsx
+++ frontend/src/store/actions/authActions.jsx
@@ -1,6 +1,10 @@
 import * as api from "../../api";
-import { openAlertMessage } from "./alertActions";
+// import { openAlertMessage } from "./alertActions";
 
+
+
+
+//  This defines a constant object authActions with a single property SET_USER_DETAILS.This property holds a string that will be used to identify the action type when dispatching it.
 export const authActions = {
   SET_USER_DETAILS: "AUTH.SET_USER_DETAILS",
 };
@@ -8,6 +12,10 @@ export const authActions = {
 
 
 // This function returns an object with login and register methods that dispatch their respective actions.
+
+/**
+ * This function returns an object with login and register methods. These methods take userDetails and history as arguments and dispatch the corresponding action creators (login and register) with these arguments. This is a common pattern in Redux to bind action creators to the dispatch function for easier use in components.
+ *  */
 export const getActions = (dispatch) => {
   return {
     login: (userDetails, history) => dispatch(login(userDetails, history)),
@@ -17,6 +25,14 @@ export const getActions = (dispatch) => {
 };
 
 
+
+
+
+/**
+ * This is a simple action creator that returns an object with the type property set to authActions.SET_USER_DETAILS and the userDetails property containing the user details. This action will be dispatched to update the Redux store with the user's information
+ * 
+ * 
+ */
 // This function returns an object with login and register methods that dispatch their respective actions.
 const setUserDetails = (userDetails) => {
   return {
@@ -31,16 +47,22 @@ const setUserDetails = (userDetails) => {
 // Calls the login API
 // Dispatches an alert message if there's an error
 // If successful, stores user details in localStorage, dispatches the setUserDetails action, and navigates to the dashboard
+
+// * These are async action creators that handle the login and registration processes, respectively.
 const login = (userDetails, history) => {
   return async (dispatch) => {
+    // * They call the corresponding API functions(api.login or api.register) to make the network request.
     const response = await api.login(userDetails);
     console.log(response);
     if (response.error) {
       dispatch(openAlertMessage(response?.exception?.response?.data));
     } else {
+
+      // If the API call is successful, they store the user details in local storage,
       const { userDetails } = response?.data;
       localStorage.setItem("user", JSON.stringify(userDetails));
 
+      // * dispatch the setUserDetails action to update the Redux store, and navigate to the /dashboard page using the history object.
       dispatch(setUserDetails(userDetails));
       history.push("/dashboard");
     }
