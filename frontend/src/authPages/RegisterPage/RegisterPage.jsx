db605d16cffe84d8ce3545c6c17ca0a653547917
Kate2dev <eseemoefe@gmail.com>
2024-10-08 11:43:29 +0100

    **Implement RegisterPage component with form validation and registration handling**

---
+++ frontend/src/authPages/RegisterPage/RegisterPage.jsx
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import { Typography } from "@mui/material";
+import AuthBox from "../../shared/components/AuthBox";
+import RegisterPageInputs from "./RegisterPageInputs";
+import RegisterPageFooter from "./RegisterPageFooter";
+import { validateRegisterForm } from "../../shared/utils/validators";
+import { connect } from "react-redux";
+import { getActions } from "../../store/actions/authActions";
+import { useNavigate } from "react-router-dom";
+
+const RegisterPage = ({ register }) => {
+  const navigate = useNavigate(); // Initialize navigate
+
+  const [mail, setMail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [isFormValid, setIsFormValid] = useState(false);
+
+  const handleRegister = () => {
+    const userDetails = {
+      mail,
+      password,
+      username,
+    };
+
+    register(userDetails, navigate); // Use navigate instead of history
+  };
+
+  useEffect(() => {
+    setIsFormValid(
+      validateRegisterForm({
+        mail,
+        username,
+        password,
+      })
+    );
+  }, [mail, username, password]);
+
+  return (
+    <AuthBox>
+      <Typography variant="h5" sx={{ color: "white" }}>
+        Create an account
+      </Typography>
+      <RegisterPageInputs
+        mail={mail}
+        setMail={setMail}
+        username={username}
+        setUsername={setUsername}
+        password={password}
+        setPassword={setPassword}
+      />
+      <RegisterPageFooter
+        handleRegister={handleRegister}
+        isFormValid={isFormValid}
+      />
+    </AuthBox>
+  );
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return getActions(dispatch); // Use getActions to return the action creators
+};
+
+export default connect(null, mapDispatchToProps)(RegisterPage);
