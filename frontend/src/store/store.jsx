e31156df3b7275afe5b0c0f04dafbc48b3e1015c
Kate2dev <eseemoefe@gmail.com>
2024-10-11 10:54:56 +0100

    comments and alert reducer was added to dispatch the alert notification features...

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -11,9 +11,11 @@ import { combineReducers, createStore, applyMiddleware } from 'redux';
 import { thunk } from 'redux-thunk';
 
 import authReducer from './reducers/authReducer'
+import alertReducer from './reducers/alertReducer'
 // combineReducers is used to create a rootReducer by combining all individual reducers. Here, you're only using one reducer, authReducer, to manage the auth slice of the state.
 const rootReducer = combineReducers({
-    auth: authReducer
+    auth: authReducer,
+    alert: alertReducer
 });
 
 
@@ -22,8 +24,6 @@ createStore creates the Redux store by passing in:
 The rootReducer (which combines all your individual reducers).
 composeWithDevTools wraps applyMiddleware(thunk) to enable the Redux DevTools extension and allow middleware (thunk) to be used.
 applyMiddleware(thunk) applies the redux-thunk middleware to the store. This allows your action creators to return functions (thunks) to handle asynchronous logic (like API calls).
-
-
 */
 
 const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
