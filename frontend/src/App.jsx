561fb6a9b6b06b8f3f5f68382883adb84eddd62d
Kate2dev <eseemoefe@gmail.com>
2024-10-11 09:54:45 +0100

    alert notification component was added to app.jsx as global notification component for the system

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,22 +2,25 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import './App.css'
 import LoginPage from './authPages/LoginPage/LoginPage';
 import Dashboard from './Dashboard/Dashboard';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
+import AlertNotification from '../src/shared/components/AlertNotification.jsx';
 
 function App() {
-  
+
 
   return (
     <>
-     <Router>
-  <Routes>
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/register" element={<LoginPage />} />
-    <Route path="/dashboard" element={<Dashboard />} />
-    
-    {/* Redirect from the root path to /dashboard */}
-    <Route path="/" element={<Navigate to="/dashboard" />} />
-  </Routes>
-</Router>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+
+          {/* Redirect from the root path to /dashboard */}
+          <Route path="/" element={<Navigate to="/dashboard" />} />
+        </Routes>
+      </Router>
+      <AlertNotification />
     </>
   )
 }
