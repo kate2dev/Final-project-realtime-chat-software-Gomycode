6425df613341b443de2c24621a981c3cf4b3597e
Kate2dev <eseemoefe@gmail.com>
2024-11-06 11:16:01 +0100

    imported the dropdownmenu here ...

--- frontend/src/Dashboard/AppBar/AppBar.jsx
+++ frontend/src/Dashboard/AppBar/AppBar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { styled } from "@mui/system";
-// import DropdownMenu from "./DropdownMenu";
+import DropdownMenu from "./DropdownMenu";
 
 const MainContainer = styled("div")({
     position: "absolute",
