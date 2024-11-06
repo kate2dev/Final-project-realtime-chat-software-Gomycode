0b1c972112e90de178ce420455659c9eb0fe3d2e
Kate2dev <eseemoefe@gmail.com>
2024-11-06 11:36:06 +0100

    **Here’s a commit message for your code:**
    ```
    Add SideBar component
    
    - Creates a styled `MainContainer` for the sidebar.
    - Renders the `MainPageButton` within the container.
    - Styles the container with specified dimensions, flex layout, and background color.
    ```

--- frontend/src/Dashboard/SideBar/SideBar.jsx
+++ frontend/src/Dashboard/SideBar/SideBar.jsx
@@ -3,16 +3,20 @@ import { styled } from "@mui/system";
 import MainPageButton from "./MainPageButton";
 
 const MainContainer = styled("div")({
-    flexGrow: 1,
-    backgroundColor: "#36393f",
-    marginTop: "48px",
-    display: "flex",
+  width: "72px",
+  height: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  backgroundColor: "#202225",
 });
 
-const Messenger = () => {
-    return <MainContainer>
-        <MainPageButton />
-    </MainContainer>;
+const SideBar = () => {
+  return (
+    <MainContainer>
+      <MainPageButton />
+    </MainContainer>
+  );
 };
 
-export default Messenger;
+export default SideBar;
