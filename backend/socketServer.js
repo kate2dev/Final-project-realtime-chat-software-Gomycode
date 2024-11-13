74527522039d770164b7e3bb01bd7302c957db4e
Kate2dev <eseemoefe@gmail.com>
2024-11-13 12:29:00 +0100

    added comments so that the codebase can be more readable for other developers...

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -4,6 +4,9 @@ const disconnectHandler = require("./socketHandlers/disconnectHandler");
 
 const serverStore = require("./serverStore");
 
+
+
+// **  Creates a Socket.IO server instance (io) attached to the provided HTTP server (server). It also sets up Cross-Origin Resource Sharing (CORS) to allow connections from potentially different origins 
 const registerSocketServer = (server) => {
   const io = require("socket.io")(server, {
     cors: {
@@ -18,23 +21,34 @@ const registerSocketServer = (server) => {
     authSocket(socket, next);
   });
 
+
+  // **  This retrieves a list of online users from the serverStore and broadcasts it to all connected clients using the "online-users" event.
+
+
   const emitOnlineUsers = () => {
     const onlineUsers = serverStore.getOnlineUsers();
     io.emit("online-users", { onlineUsers });
   };
 
+  // This listens for the "connection" event emitted when a user connects. Here's what happens when a user connects:
   io.on("connection", (socket) => {
     console.log("user connected");
     console.log(socket.id);
 
     newConnectionHandler(socket, io);
+
+    // Calls emitOnlineUsers to broadcast the updated list of online users, including the new user
     emitOnlineUsers();
 
+
+    // Calls emitOnlineUsers to broadcast the updated list of online users, including the new user.
     socket.on("disconnect", () => {
       disconnectHandler(socket);
     });
   });
 
+
+  // **  Sets an interval that calls emitOnlineUsers every 8 seconds. This keeps the list of online users updated and broadcast to connected clients periodically.
   setInterval(() => {
     emitOnlineUsers();
   }, [1000 * 8]);
