74b7770b9b76ac497e3a1916da4e7c7f290861a3
Kate2dev <eseemoefe@gmail.com>
2024-11-22 03:07:49 +0100

    feat(ui): add PendingInvitationsList component with Redux integration
    - Implement `PendingInvitationsList` component to display pending friend invitations:
      - Maps through `pendingFriendsInvitations` to render `PendingInvitationsListItem` components.
      - Displays sender's username and email for each invitation.
    
    - Style the component using a `styled` container with a responsive design.
    
    - Integrate Redux:
      - Connect the component to the Redux store using `connect` and `mapStoreStateToProps`.
      - Map `pendingFriendsInvitations` from the `friends` state.

--- frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.jsx
+++ frontend/src/Dashboard/FriendsSideBar/PendingInvitationsList/PendingInvitationsList.jsx
@@ -1,46 +1,36 @@
 
 import { styled } from "@mui/system";
 import PendingInvitationsListItem from "./PendingInvitationsListItem";
-
-const DUMMY_INVITATIONS = [
-    {
-        _id: "1",
-        senderId: {
-            username: "Mark",
-            mail: "dummy@ad.com",
-        },
-    },
-    {
-        _id: "2",
-        senderId: {
-            username: "John",
-            mail: "John@ad.com",
-        },
-    },
-];
+import { connect } from "react-redux";
 
 const MainContainer = styled("div")({
-    width: "100%",
-    height: "22%",
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-    overflow: "auto",
+  width: "100%",
+  height: "22%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  overflow: "auto",
 });
 
-const PendingInvitationsList = () => {
-    return (
-        <MainContainer>
-            {DUMMY_INVITATIONS.map((invitation) => (
-                <PendingInvitationsListItem
-                    key={invitation._id}
-                    id={invitation._id}
-                    username={invitation.senderId.username}
-                    mail={invitation.senderId.mail}
-                />
-            ))}
-        </MainContainer>
-    );
+const PendingInvitationsList = ({ pendingFriendsInvitations }) => {
+  return (
+    <MainContainer>
+      {pendingFriendsInvitations.map((invitation) => (
+        <PendingInvitationsListItem
+          key={invitation._id}
+          id={invitation._id}
+          username={invitation.senderId.username}
+          mail={invitation.senderId.mail}
+        />
+      ))}
+    </MainContainer>
+  );
 };
 
-export default PendingInvitationsList;
+const mapStoreStateToProps = ({ friends }) => {
+  return {
+    ...friends,
+  };
+};
+
+export default connect(mapStoreStateToProps)(PendingInvitationsList);
