090c7f38cfd84cd2c255b31338b5d04c57588865
Kate2dev <eseemoefe@gmail.com>
2024-11-22 03:43:57 +0100

    feat(friendsList): display friends with online status
    - Implement `checkOnlineUsers` function to check which friends are online.
    - Display friends in `FriendsList` component with their online status.
    - Integrated with Redux to fetch the `friends` and `onlineUsers` state.
    - Rendered `FriendsListItem` for each friend, showing their online/offline status.

--- frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.jsx
+++ frontend/src/Dashboard/FriendsSideBar/FriendsList/FriendsList.jsx
@@ -1,43 +1,41 @@
-import React from "react";
+
 import { styled } from "@mui/system";
 import FriendsListItem from "./FriendsListItem";
-
-const DUMMY_FRIENDS = [
-    {
-        id: 1,
-        username: "Mark",
-        isOnline: true,
-    },
-    {
-        id: 2,
-        username: "Anna",
-        isOnline: false,
-    },
-    {
-        id: 3,
-        username: "John",
-        isOnline: false,
-    },
-];
+import { connect } from "react-redux";
 
 const MainContainer = styled("div")({
-    flexGrow: 1,
-    width: "100%",
+  flexGrow: 1,
+  width: "100%",
 });
 
-const FriendsList = () => {
-    return (
-        <MainContainer>
-            {DUMMY_FRIENDS.map((f) => (
-                <FriendsListItem
-                    username={f.username}
-                    id={f.id}
-                    key={f.id}
-                    isOnline={f.isOnline}
-                />
-            ))}
-        </MainContainer>
-    );
+const checkOnlineUsers = (friends = [], onlineUsers = []) => {
+  friends.forEach((f) => {
+    const isUserOnline = onlineUsers.find((user) => user.userId === f.id);
+    f.isOnline = isUserOnline ? true : false;
+  });
+
+  return friends;
 };
 
-export default FriendsList;
+const FriendsList = ({ friends, onlineUsers }) => {
+  return (
+    <MainContainer>
+      {checkOnlineUsers(friends, onlineUsers).map((f) => (
+        <FriendsListItem
+          username={f.username}
+          id={f.id}
+          key={f.id}
+          isOnline={f.isOnline}
+        />
+      ))}
+    </MainContainer>
+  );
+};
+
+const mapStoreStateToProps = ({ friends }) => {
+  return {
+    ...friends,
+  };
+};
+
+export default connect(mapStoreStateToProps)(FriendsList);
