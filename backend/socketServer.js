a5f78634e2d6b22b7a3f16f9c0bc9b79f22732d2
Kate2dev <eseemoefe@gmail.com>
2024-11-25 11:17:49 +0100

    feat(socket): add event handlers for direct messaging and chat history
    - Added `direct-message` event handler to process incoming direct messages.
    - Added `direct-chat-history` event handler to retrieve and send chat history.

--- backend/socketServer.js
+++ backend/socketServer.js
@@ -1,12 +1,11 @@
 const authSocket = require("./middleware/authSocket");
 const newConnectionHandler = require("./socketHandlers/newConnectionHandler");
 const disconnectHandler = require("./socketHandlers/disconnectHandler");
+const directMessageHandler = require("./socketHandlers/directMessageHandler");
+const directChatHistoryHandler = require("./socketHandlers/directChatHistoryHandler");
 
 const serverStore = require("./serverStore");
 
-
-
-// **  Creates a Socket.IO server instance (io) attached to the provided HTTP server (server). It also sets up Cross-Origin Resource Sharing (CORS) to allow connections from potentially different origins 
 const registerSocketServer = (server) => {
   const io = require("socket.io")(server, {
     cors: {
@@ -21,34 +20,31 @@ const registerSocketServer = (server) => {
     authSocket(socket, next);
   });
 
-
-  // **  This retrieves a list of online users from the serverStore and broadcasts it to all connected clients using the "online-users" event.
-
-
   const emitOnlineUsers = () => {
     const onlineUsers = serverStore.getOnlineUsers();
     io.emit("online-users", { onlineUsers });
   };
 
-  // This listens for the "connection" event emitted when a user connects. Here's what happens when a user connects:
   io.on("connection", (socket) => {
     console.log("user connected");
     console.log(socket.id);
 
     newConnectionHandler(socket, io);
-
-    // Calls emitOnlineUsers to broadcast the updated list of online users, including the new user
     emitOnlineUsers();
 
+    socket.on("direct-message", (data) => {
+      directMessageHandler(socket, data);
+    });
 
-    // Calls emitOnlineUsers to broadcast the updated list of online users, including the new user.
+    socket.on("direct-chat-history", (data) => {
+      directChatHistoryHandler(socket, data);
+    });
+
     socket.on("disconnect", () => {
       disconnectHandler(socket);
     });
   });
 
-
-  // **  Sets an interval that calls emitOnlineUsers every 8 seconds. This keeps the list of online users updated and broadcast to connected clients periodically.
   setInterval(() => {
     emitOnlineUsers();
   }, [1000 * 8]);
