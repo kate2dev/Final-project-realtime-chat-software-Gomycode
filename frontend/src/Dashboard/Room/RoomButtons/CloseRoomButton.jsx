28c8459322b7c1b894e3dd79f2f723ee4e3b006a
Kate2dev <eseemoefe@gmail.com>
2024-11-27 12:40:45 +0100

    feat(ui): Add CloseRoomButton component to leave the room
    - Introduced `CloseRoomButton` React component.
    - Allows users to leave the room using the `leaveRoom` function from `roomHandler`.
    - Utilizes `CloseIcon` from Material-UI for the button icon.
    - Triggers room exit on button click via `handleLeaveRoom` function.

--- frontend/src/Dashboard/Room/RoomButtons/CloseRoomButton.jsx
+++ frontend/src/Dashboard/Room/RoomButtons/CloseRoomButton.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
 import * as roomHandler from "../../../realtimeCommunication/roomHandler";
